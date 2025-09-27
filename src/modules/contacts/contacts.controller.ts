3b66ac8290e04942779a8eb6c67872c8fc6bd4f7
Jander Gomez <jandergb.30gmail.com>
2025-09-27 09:19:32 +0200

    feat: update branch from develop

--- src/modules/contacts/contacts.controller.ts
+++ src/modules/contacts/contacts.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { ContactsService } from './contacts.service';
 import { CreateContactDto } from './dto/create-contact.dto';
-import { UpdateContactDto } from './dto/update-contact.dto';
+// import { UpdateContactDto } from './dto/update-contact.dto';
 
 @Controller('contacts')
 export class ContactsController {
@@ -22,10 +22,10 @@ export class ContactsController {
     return this.contactsService.findOne(+id);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateContactDto: UpdateContactDto) {
-    return this.contactsService.update(+id, updateContactDto);
-  }
+  // @Patch(':id')
+  // update(@Param('id') id: string, @Body() updateContactDto: UpdateContactDto) {
+  //   return this.contactsService.update(+id, updateContactDto);
+  // }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
--- src/modules/contacts/contacts.service.ts
+++ src/modules/contacts/contacts.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { CreateContactDto } from './dto/create-contact.dto';
-import { UpdateContactDto } from './dto/update-contact.dto';
+// import { UpdateContactDto } from './dto/update-contact.dto';
 
 @Injectable()
 export class ContactsService {
@@ -16,9 +16,9 @@ export class ContactsService {
     return `This action returns a #${id} contact`;
   }
 
-  update(id: number, updateContactDto: UpdateContactDto) {
-    return `This action updates a #${id} contact`;
-  }
+  // update(id: number, updateContactDto: UpdateContactDto) {
+  //   return `This action updates a #${id} contact`;
+  // }
 
   remove(id: number) {
     return `This action removes a #${id} contact`;
